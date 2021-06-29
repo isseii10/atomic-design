1c8cc79188459488e42e2f695342f5a08e3ddcd7
issei <63489981+itss10@users.noreply.github.com>
2021-06-30 04:24:25 +0900

    organism

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import "./styles.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molcules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+
+const user = {
+  name: "issei",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "1234@example.com",
+  phone: "090-1111-1111",
+  company: {
+    name: "株式会社AF"
+  },
+  website: "https://google.com"
+};
+
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
       <SearchInput placeholder="検索条件を入力" />
+      <UserCard user={user} />
     </div>
   );
 }

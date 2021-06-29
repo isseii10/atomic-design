ed4f5536a7f565767d21a75825137b71b113ff9b
issei <63489981+itss10@users.noreply.github.com>
2021-06-30 02:08:30 +0900

    molecules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molcules/SearchInput";
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>テスト</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <SearchInput placeholder="検索条件を入力" />
     </div>
   );
 }
